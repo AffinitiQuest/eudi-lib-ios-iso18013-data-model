b89723b74095628d0b7367332cac7d2098d79e6d
Ian Carbone <ian@affinitiquest.io>
2025-06-27 08:35:30 -0400

    Removing comment.

--- Sources/MdocDataModel18013/MdocResponse/DeviceResponse.swift
+++ Sources/MdocDataModel18013/MdocResponse/DeviceResponse.swift
@@ -18,28 +18,6 @@ import Foundation
 import SwiftCBOR
 import OrderedCollections
 
-
-//public struct W3CDeviceResponse: Sendable {
-//    public let version: String
-//    public static let defaultVersion = "1.0"
-//    public let documents: [W3CDocument]?
-//    public let documentErrors: [DocumentError]?
-//    public let status: UInt64
-//    
-//    enum Keys: String {
-//        case version
-//        case documents
-//        case documentErrors
-//        case status
-//    }
-//
-//    public init(version: String? = nil, documents: [W3CDocument]? = nil, documentErrors: [DocumentError]? = nil, status: UInt64) {
-//        self.version = version ?? Self.defaultVersion
-//        self.documents = documents
-//        self.documentErrors = documentErrors
-//        self.status = status
-//    }
-//}
 /// Device retrieval mdoc response. It is CBOR encoded
 ///
 /// In mdoc reader initialize from CBOR data received from holder (data exchange)
@@ -74,35 +52,6 @@ public struct DeviceResponse: Sendable {
 		self.status = status
 	}
 }
-
-//extension W3CDeviceResponse: CBORDecodable {
-//    public init?(cbor: CBOR) {
-//        guard case .map(let cd) = cbor else { return nil }
-//        guard case .utf8String(let v) = cd[Keys.version] else { return nil }
-//        version = v
-//        if case let .array(ar) = cd[Keys.documents] {
-//            let ds = ar.compactMap { W3CDocument(cbor:$0) }
-//            if ds.count > 0 { documents = ds } else { documents = nil }
-//        } else { documents = nil }
-//        if case let .array(are) = cd[Keys.documentErrors] {
-//            let de = are.compactMap { DocumentError(cbor:$0) }
-//            if de.count > 0 { documentErrors = de } else { documentErrors = nil }
-//        }  else { documentErrors = nil }
-//        guard case .unsignedInt(let st) = cd[Keys.status] else { return nil }
-//        status = st
-//    }
-//}
-//
-//extension W3CDeviceResponse: CBOREncodable {
-//    public func toCBOR(options: CBOROptions) -> CBOR {
-//        var cbor = OrderedDictionary<CBOR, CBOR>()
-//        cbor[.utf8String(Keys.version.rawValue)] = .utf8String(version)
-//        if let ds = documents { cbor[.utf8String(Keys.documents.rawValue)] = ds.toCBOR(options: options) }
-//        if let de = documentErrors { cbor[.utf8String(Keys.documentErrors.rawValue)] = .array(de.map {$0.toCBOR(options: options)}) }
-//        cbor[.utf8String(Keys.status.rawValue)] = .unsignedInt(status)
-//        return .map(cbor)
-//    }
-//}
 
 extension DeviceResponse: CBORDecodable {
 	public init?(cbor: CBOR) {
